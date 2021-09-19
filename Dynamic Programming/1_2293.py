456bf8f213db67ce65700b19a4d2b35630ca1745
codehyunn <87523224+codehyunn@users.noreply.github.com>
2021-09-19 23:32:50 +0900

    Add files via upload

---
+++ Dynamic Programming/1_2293.py
@@ -0,0 +1,17 @@
+#2293 동전 1
+
+n,k = map(int,input().split())
+p = [int(input())for _ in range(n)]
+dp = [0 for _ in range(k+1)] 
+
+
+for j in p:
+    for i in range(j, k+1):
+        if i == j:
+            dp[i] = dp[i] + 1
+        else:
+            dp[i] = dp[i] + dp[i-j]
+        
+ans = dp[k]
+print(ans)
+
