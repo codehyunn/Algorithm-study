64770466515f961399926a5d7b79968cd96fbcfc
Jihyun Kim <87523224+codehyunn@users.noreply.github.com>
2022-12-29 20:35:10 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/README.md
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/README.md
@@ -1,6 +1,6 @@
 # [level 1] 두 정수 사이의 합 - 12912 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12912) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12912#) 
 
 ### 성능 요약
 
--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
@@ -1,10 +1,4 @@
 def solution(a, b):
     x = abs(a-b) + 1
-    if x == 1 :
-        answer = a
-    elif x % 2 == 0 :
-        answer = (a+b) * (x/2)
-    else :
-        answer = (a+b)//2 * x
-        
+    answer = (a+b) * x/2
     return answer
