12875f92adee3f096fd595b7831aa9fe1f61e246
Jihyun Kim <87523224+codehyunn@users.noreply.github.com>
2023-01-01 13:37:15 +0900

    [level 1] Title: 폰켓몬, Time: 0.18 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,7 +1,2 @@
 def solution(nums):
-    setnum = set(nums)
-    if len(setnum) >= len(nums)//2 :
-        answer = len(nums)//2
-    else :
-        answer = len(setnum)
-    return answer
+    return min(len(set(nums)), len(nums)/2)
