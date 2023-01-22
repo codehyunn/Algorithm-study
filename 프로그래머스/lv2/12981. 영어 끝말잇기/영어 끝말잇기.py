60f576364acbdcb7dc35d9fdbf9cdcc12c3b5793
Jihyun Kim <87523224+codehyunn@users.noreply.github.com>
2023-01-22 20:07:33 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,12 +1,10 @@
 def solution(n, words):
-    answer = [0,0]
     used_words = []
     for idx, word in enumerate(words) :
         if word in used_words \
             or (len(used_words) > 0 and used_words[-1][-1] != word[0]) :
-            answer = [idx%n+1,idx//n+1]
-            break
+            return [idx%n+1,idx//n+1]
         else :
             used_words.append(word)
             
-    return answer
+    return [0,0]
