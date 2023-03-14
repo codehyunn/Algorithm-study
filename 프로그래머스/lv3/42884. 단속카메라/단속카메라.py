00bd6c10ccf70f759de3259526e4f88f74c01a70
Jihyun Kim <87523224+codehyunn@users.noreply.github.com>
2023-03-14 15:39:15 +0900

    [level 3] Title: 단속카메라, Time: 14.83 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
@@ -0,0 +1,13 @@
+def solution(routes):
+    routes = sorted(routes, key = lambda x : x[1])
+    times = [[-30000,30000]]
+    
+    for i in range(len(routes)) :
+        for t in range(len(times)) :
+            if routes[i][0] <= times[t][1] :
+                times[t][0], times[t][1] = routes[i][0], min(times[t][1], routes[i][1])
+                break
+        else :
+            times.append(routes[i])
+    
+    return len(times)
