3c977336347277e8fb7d3256f4b4fd277390bb9e
Jihyun Kim <87523224+codehyunn@users.noreply.github.com>
2023-03-28 14:07:01 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.41 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,17 +1,20 @@
 def solution(survey, choices):
-    result = {0 : {'R' : 0, 'T' : 0}, 1 : {'C' : 0, 'F' : 0},
-              2 : {'J' : 0, 'M' : 0}, 3 : {'A' : 0, 'N' : 0}}
-    maplist = [['R','T'], ['C','F'],['J','M'],['A','N']]
-    answer = ''
+    idxs = {'R':0, 'T':0, 'C':1, 'F':1, 'J':2, 'M':2, 'A':3, 'N':3}
+    results = [{'R':0,'T':0},{'C':0,'F':0},{'J':0,'M':0},{'A':0,'N':0}]
     
-    for s,c in list(zip(survey, choices)) :
-        key1 = maplist.index(sorted(list(s)))
-        key2 = s[c//4]
-        w = c%4 if c//4 == 1 else 4-c%4
-        result[key1][key2] += w
+    for strs, choice in zip(survey, choices) :
+        char = strs[choice//4]
+        score = choice%4 if choice//4 == 1 else 4-choice
+        results[idxs[char]][char] += score
     
-    for r in result.items() :
-        a = sorted(r[1].items(),reverse=True, key=lambda x : x[1])
-        answer += a[0][0]
-             
+    answer = ""
+    for result in results :
+        value = list(result.values())
+        key = result.keys()
+
+        if value[0] == value[1] :
+            answer += min(key)
+        else :
+            answer += max(key, key=lambda x : result[x])
+        
     return answer
