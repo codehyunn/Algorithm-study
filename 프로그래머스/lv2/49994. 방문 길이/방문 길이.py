1afc19b58411ecee4f01e00f461a54341b670188
Jihyun Kim <87523224+codehyunn@users.noreply.github.com>
2023-04-06 23:38:53 +0900

    [level 2] Title: 방문 길이, Time: 1.63 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.py
@@ -1,25 +1,29 @@
 def solution(dirs) : 
+    move_dict= {'R':0, 'L':1, 'U':2, "D":3}
+    dx, dy = [0,0,1,-1],[1,-1,0,0]
     visited_pairs = []
-    move_dict = {'R':0, 'L':1, 'U':2, "D":3}
-    dx, dy  = [0,0,1,-1], [1,-1,0,0]
 
     count = 0
-    prev_x, prev_y = 0,0
+    prev = (0,0)
 
     for d in dirs :
         idx = move_dict[d]
-        x,y = prev_x+dx[idx], prev_y+dy[idx]
 
-        if -5 <= x <= 5 and -5<=y<=5 :
-            if prev_x < x or prev_y < y: 
-                pair = (prev_x, prev_y, x, y)
+        # 위치 이동 
+        x,y = prev[0]+dx[idx], prev[1]+dy[idx]
+        now = (x,y)
+
+        # 이동한 위치(x,y)에 대한 검증 진행
+        if -5<=x<=5 and -5<=y<=5:
+            if prev < now: # (x,y,x',y')
+                pair = prev + now 
             else :
-                pair = (x,y,prev_x,prev_y)
+                pair = now + prev
             
             if pair not in visited_pairs :
                 count += 1
                 visited_pairs.append(pair)
 
-            prev_x, prev_y = x,y
+            prev = now
 
     return count
