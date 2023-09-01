096cb54d1d3ca413fc93148cd630b3c409c4bc15
Jihyun Kim <87523224+codehyunn@users.noreply.github.com>
2023-09-01 22:31:51 +0900

    [Gold V] Title: 퇴사 2, Time: 3556 ms, Memory: 274908 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -1,25 +1,26 @@
 import sys
-import heapq
 
 #sys.stdin = open('answer.txt', 'r')
 input = sys.stdin.readline
 
 n = int(input())
+data = [[0,0]]
 
-queue = []
 for i in range(n) :
-    t,p = map(int, input().split())
-    heapq.heappush(queue, (i+t, i+1, p))
-    
-cost = [[0,0] for _ in range(n+1)]
+    t, p = map(int, input().split())
+    data.append([t,p])
 
-for i in range(1,n+1) :
-    cost[i][0] = max(cost[i-1])
+cost = [0 for _ in range(n+1)]
+
+for i in range(1, n+1) :
+    t, p = data[i]
+    fin_day = i+t-1
     
-    while queue :
-        if queue[0][0] != i :
-            break
-        _, start, money = heapq.heappop(queue)
-        cost[i][1] = max(cost[i][1], cost[start][0]+money)
- 
-print(max(cost[-1]))
+    cost[i] = max(cost[i], cost[i-1])
+
+    if fin_day > n :
+        continue
+    
+    cost[fin_day] = max(cost[fin_day], cost[fin_day-1], p+cost[i-1])
+    
+print(cost[-1])
