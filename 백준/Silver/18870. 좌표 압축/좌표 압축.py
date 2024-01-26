f8cba6a4e126d17de8aad088d8e3a46befb476d8
Jihyun Kim <wlgus01110@gmail.com>
2024-01-26 14:48:45 +0900

    [Silver II] Title: 좌표 압축, Time: 1896 ms, Memory: 143268 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,17 +1,13 @@
 import sys 
-from collections import defaultdict
 
 input = sys.stdin.readline
 
 n = int(input())
 nums = list(map(int,input().split()))
 
-idxs = defaultdict(list)
-for i in range(n) :
-    idxs[nums[i]].append(i)
+value_dict = {num : i for i,num in enumerate(sorted(set(nums)))}
 
-for i, num in enumerate(sorted(set(nums))) :
-    for idx in idxs[num] :
-        nums[idx] = i
+for i, num in enumerate(nums) :
+    nums[i] = value_dict[num]
 
 print(* nums)
