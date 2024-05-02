e4ed2023803c6d497a42b9e294fd0030a24160a1
Jihyun Kim <wlgus01110@gmail.com>
2024-05-02 14:26:23 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,7 +1,7 @@
 def solution(citations):
-    citations = sorted(citations,reverse=True)
-    point = 0
+    citations.sort(reverse=True)
     
+    point = 0
     for h in range(citations[0], -1, -1) :
         if point+1 < len(citations) and citations[point+1] >= h :
             point += 1
