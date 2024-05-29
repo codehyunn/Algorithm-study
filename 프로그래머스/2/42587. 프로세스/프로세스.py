8fdd6305c62b3ced8b716694879e172c52a460d6
Jihyun Kim <wlgus01110@gmail.com>
2024-05-29 17:24:54 +0900

    [level 2] Title: 프로세스, Time: 0.13 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+def solution(priorities, location):
+    n_process = len(priorities)
+    answer = []
+    
+    priorities = deque(priorities)
+    order = deque([i for i in range(1, n_process+1)])
+        
+    while len(answer) < n_process :
+        x = priorities.popleft()
+        y = order.popleft()
+        
+        flag = False
+        for priority in priorities :
+            if priority > x :
+                flag = True
+                break
+        
+        if flag :
+            priorities.append(x)
+            order.append(y)
+        
+        else :
+            answer.append(y)
+
+    return answer.index(location+1)+1
